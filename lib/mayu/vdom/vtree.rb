da387275ece8f234e9ee7b059136e867e85415d2
Andreas Alin <andreas.alin@gmail.com>
2022-08-01 14:30:21 -0500

    Fix appending

--- lib/mayu/vdom/vtree.rb
+++ lib/mayu/vdom/vtree.rb
@@ -666,15 +666,8 @@ module Mayu
       sig {params(str1: String, str2: String).returns(T.nilable(String))}
       def append_part(str1, str2)
         return nil if str1.strip.empty? || str1.length >= str2.length
-
-        equal_length = str1.chars
-          .zip(str2.chars.to_a.slice(0, str1.length) || [])
-          .take_while { |c1, c2| c1 == c2 }
-          .length
-
-        return nil if equal_length.zero?
-
-        str2.slice(equal_length..-1)
+        return nil unless str2.slice(0..str1.length) == str1
+        str2.slice(str1.length..-1)
       end
     end
   end
