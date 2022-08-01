c79ede2d8e931d1304c055d0aab95fa553316ba4
Andreas Alin <andreas.alin@gmail.com>
2022-08-01 17:29:10 -0500

    It seems to work very well

--- markup2.rb
+++ markup2.rb
@@ -898,7 +898,7 @@ class MyApp < VDOM2::Component
   def render
     h(:ul) do
       props[:items].map do |item|
-        h(:li, key: item[:id]) { item[:title] }
+        h(item[:type] || :li, key: item[:id]) { item[:title] }
       end
     end
   end
@@ -943,7 +943,7 @@ hax(
   items: [
     { id: 0, title: "Item 0" },
     { id: 1, title: "Item 1" },
-    { id: 3, title: "Item 3" },
+    { id: 3, title: "Item 3", type: :p },
     { id: 4, title: "Item 4" },
     { id: 2, title: "Item 2" },
     { id: 5, title: "Item 5" },
@@ -964,26 +964,56 @@ hax(
 
 hax(
   items: [
-    { id: 0, title: "Item 0" },
-    { id: 1, title: "Item 1" },
-    { id: 2, title: "Item 2" },
-    { id: 3, title: "Item 3" },
     { id: 4, title: "Item 4" },
-    { id: 5, title: "Item 5" },
-    { id: 6, title: "Item 6" },
-    { id: 7, title: "Item 7" },
-    { id: 8, title: "Item 8" },
-    { id: 9, title: "Item 9" },
-    { id: 10, title: "Item 10" },
-    { id: 11, title: "Item 11" },
-    { id: 12, title: "Item 12" },
     { id: 13, title: "Item 13" },
-    { id: 14, title: "Item 14" },
+    { id: 7, title: "Item 7" },
+    { id: 12, title: "Item 12" },
+    { id: 9, title: "Item 9" },
     { id: 15, title: "Item 15" },
-    { id: 16, title: "Item 16" },
-    { id: 17, title: "Item 17" },
-    { id: 18, title: "Item 18" },
     { id: 19, title: "Item 19" },
+    { id: 6, title: "Item 6" },
+    { id: 2, title: "Item 2" },
+    { id: 11, title: "Item 11" },
+    { id: 17, title: "Item 17" },
+    { id: 3, title: "Item 3" },
+    { id: 16, title: "Item 16" },
+    { id: 18, title: "Item 18" },
+    { id: 8, title: "Item 8" },
+    { id: 0, title: "Item 0" },
+    { id: 14, title: "Item 14" },
+    { id: 1, title: "Item 1" },
+    { id: 10, title: "Item 10" },
+    { id: 5, title: "Item 5" },
+  ]
+)
+
+hax(
+  items: [
+    { id: 30, title: "Item 30" },
+    { id: 16, title: "Item 16" },
+    { id: 24, title: "Item 24" },
+    { id: 14, title: "Item 14" },
+    { id: 18, title: "Item 18" },
+    { id: 21, title: "Item 21" },
+    { id: 22, title: "Item 22" },
+    { id: 13, title: "Item 13" },
+    { id: 17, title: "Item 17" },
+    { id: 19, title: "Item 19" },
+    { id: 15, title: "Item 15" },
+    { id: 26, title: "Item 26" },
+    { id: 20, title: "Item 20" },
+    { id: 27, title: "Item 27" },
+    { id: 28, title: "Item 28" },
+    { id: 32, title: "Item 32" },
+    { id: 31, title: "Item 31" },
+    { id: 11, title: "Item 11" },
+    { id: 9, title: "Item 9" },
+    { id: 23, title: "Item 23" },
+    { id: 8, title: "Item 8" },
+    { id: 29, title: "Item 29" },
+    { id: 10, title: "Item 10" },
+    { id: 12, title: "Item 12" },
+    { id: 25, title: "Item 25" },
   ]
 )
 
