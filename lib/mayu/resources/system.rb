1aaaaf2ef70e67fc280923db67862ee7e608c88f
Andreas Alin <andreas.alin@gmail.com>
2022-09-07 20:35:37 -0500

    Rename app/ -> pages/

--- lib/mayu/resources/system.rb
+++ lib/mayu/resources/system.rb
@@ -9,6 +9,10 @@ module Mayu
     class System
       extend T::Sig
 
+      PAGES_PATH = "/pages"
+      STORES_PATH = "/stores"
+      COMPONENTS_PATH = "/components"
+
       sig { returns(String) }
       attr_reader :root
 
@@ -17,6 +21,7 @@ module Mayu
         @root = root
         @resolver = T.let(Resolver.new(self), Resolver)
         @dependency_graph = T.let(DependencyGraph.new, DependencyGraph)
+        @resources = T.let({}, T::Hash[String, Resource])
       end
 
       sig { params(path: String).returns(T.class_of(Component::Base)) }
@@ -33,14 +38,22 @@ module Mayu
 
       sig { params(path: String).returns(Resource) }
       def load_page(path)
-        load(path, "/app")
+        load_resource(path, PAGES_PATH)
+      end
+
+      sig do
+        params(path: String, source_path: String).returns(Components::Base)
+      end
+      def load_component(path, source_path = COMPONENTS_PATH)
+        load_resource(path, source_path).type => Types::Ruby => type
+        type.class
       end
 
       sig { params(path: String, source_path: String).returns(Resource) }
-      def load(path, source_path = "/")
+      def load_resource(path, source_path = "/")
         resolved_path = @resolver.resolve(path, source_path)
 
-        Resource.load(self, resolved_path)
+        @resources[resolved_path] ||= Resource.load(self, resolved_path)
       end
     end
   end
