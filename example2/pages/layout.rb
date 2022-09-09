8995e96236fc460470ab30cf3e16c34a6a926d35
Andreas Alin <andreas.alin@gmail.com>
2022-09-09 13:57:07 -0500

    Improve layout

--- example2/pages/layout.rb
+++ example2/pages/layout.rb
@@ -2,17 +2,24 @@
 
 def render
   h.html do
-    h.head { h.meta name: "charset", value: "utf-8" }
+    h.head do
+      h.meta name: "charset", value: "utf-8"
+      h.title "Mayu Live"
+    end
 
-    h.body do
-      h.div do
-        h.menu do
+    h.body(class: styles.body) do
+      h.header(class: styles.header) do
+        h.div(class: styles.maxWidth) { h.h1 "Mayu Live", class: styles.title }
+      end
+
+      h.nav(class: styles.nav) do
+        h.menu(class: "#{styles.menu} #{styles.maxWidth}") do
           h.li { h.a "Start page", href: "/" }
           h.li { h.a "Pokemon", href: "/pokemon" }
         end
-
-        h << children.first
       end
+
+      h.main { h.section(class: styles.maxWidth) { h << children.first } }
     end
   end
 end
--- example2/pages/page.rb
+++ example2/pages/page.rb
@@ -1,5 +1,28 @@
+ITEMS = ["server side rendered apps", "very fast", "wow"]
+
+def self.get_initial_state(**props)
+  { index: 0 }
+end
+
+def mount
+  loop do
+    sleep 1
+
+    update { |state| { index: state[:index].succ % ITEMS.size } }
+  end
+end
+
 def render
   h.div class: styles.foo do
+    h.ul do
+      ITEMS.each_with_index do |item, index|
+        if index == state[:index]
+          h.li item, key: index, class: styles.active
+        else
+          h.li item, key: index
+        end
+      end
+    end
     h << "hello"
   end
 end
