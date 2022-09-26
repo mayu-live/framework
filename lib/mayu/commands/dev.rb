42b5162e8fd31f957727ed6bbfd20a39f775c9db
Andreas Alin <andreas.alin@gmail.com>
2022-09-26 02:37:14 -0500

    server2 -> server + extract classes to separate files

--- lib/mayu/commands/dev.rb
+++ lib/mayu/commands/dev.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 require_relative "base"
-require_relative "../server2"
+require_relative "../server"
 
 module Mayu
   module Commands
@@ -12,7 +12,7 @@ module Mayu
       sig { params(argv: T::Array[String]).void }
       def call(argv)
         Configuration.log_config(configuration)
-        Server2.start_dev(configuration)
+        Server.start_dev(configuration)
       end
     end
   end
