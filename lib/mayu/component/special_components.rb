1181afee19b763168fec00110a5d1f26e236cb80
Andreas Alin <andreas.alin@gmail.com>
2022-09-28 15:46:32 -0500

    Extract RE and set external="external" on external links

--- lib/mayu/component/special_components.rb
+++ lib/mayu/component/special_components.rb
@@ -38,6 +38,8 @@ module Mayu
           end
         end
 
+        EXTERNAL_LINK_RE = T.let(%r{\A[a-z0-9]+://}, Regexp)
+
         sig { params(_: T.untyped, href: String).void }
         def handle_click(_, href)
           helpers.navigate(href.to_s)
@@ -46,8 +48,8 @@ module Mayu
         sig { override.returns(T.nilable(VDOM::Descriptor)) }
         def render
           overridden_props =
-            if props[:href].to_s.match(%r{\A[a-z0-9]+://})
-              { rel: "noreferrer", **props }
+            if EXTERNAL_LINK_RE.match?(props[:href] || nil)
+              { rel: "noreferrer", external: true, **props }
             else
               { **props, on_click: NavigateHandler.new }
             end
