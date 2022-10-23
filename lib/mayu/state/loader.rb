c61ee1cd5b3135bfda71100064d377126fc34f5a
Andreas Alin <andreas.alin@gmail.com>
2022-10-22 20:03:13 -0500

    Fix type errors

--- lib/mayu/state/loader.rb
+++ lib/mayu/state/loader.rb
@@ -200,12 +200,6 @@ module Mayu
             data = ReducerBuilder.build(File.read(path), path)
             name = File.basename(path, ".*").capitalize.to_sym
 
-            Mayu::App.replace_module(
-              name,
-              Actions: data[:action_module],
-              Selectors: data[:selector_module]
-            )
-
             [
               name,
               ->(state, action) do
