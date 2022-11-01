a260e4ace401740e83de4912cbbd919cf66fef07
Andreas Alin <andreas.alin@gmail.com>
2022-10-31 20:39:57 -0500

    Fix tests

--- lib/mayu/vdom.test.rb
+++ lib/mayu/vdom.test.rb
@@ -48,12 +48,12 @@ class Mayu::VDOM::Test < Minitest::Test
       # page.debug!
 
       assert_equal(page.to_html, <<~HTML)
-        <div class="lib/mayu/vdom.foo">
+        <div class="lib/mayu/vdom.test.foo">
           <button
             name="increment"
             value="1"
             onclick="Mayu.handle(event,'LckOTvJsohBty1Pm')"
-            class="lib/mayu/vdom.increment"
+            class="lib/mayu/vdom.test.increment"
           >
             Decrement
           </button>
@@ -61,7 +61,7 @@ class Mayu::VDOM::Test < Minitest::Test
             name="decrement"
             value="-1"
             onclick="Mayu.handle(event,'LckOTvJsohBty1Pm')"
-            class="lib/mayu/vdom.decrement"
+            class="lib/mayu/vdom.test.decrement"
           >
             Increment
           </button>
