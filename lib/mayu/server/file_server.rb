c708ba66893e1749870ee9e08e80d115e79bc64d
Andreas Alin <andreas.alin@gmail.com>
2022-11-07 03:00:40 -0500

    Cache small assets in memory

--- lib/mayu/server/file_server.rb
+++ lib/mayu/server/file_server.rb
@@ -9,17 +9,34 @@ module Mayu
       class FoundFile < T::Struct
         const :absolute_path, String
         const :content_type, String
-        const :has_brotli, T::Boolean
+        const :size, Integer
       end
 
       CACHE_MAX_AGE = T.let(60 * 60 * 24 * 7, Integer)
 
+      # TODO: Make configurable. A higher value means less
+      # filsystem IO, but obviously consumes more memory.
+      DEFAULT_MEMORY_CACHE_MAX_SIZE = T.let(1024, Integer)
+
       extend T::Sig
 
-      sig { params(root_dir: String).void }
-      def initialize(root_dir)
+      sig { params(root_dir: String, memory_cache_max_size: Integer).void }
+      def initialize(
+        root_dir,
+        memory_cache_max_size: DEFAULT_MEMORY_CACHE_MAX_SIZE
+      )
         @root_dir = root_dir
-        @found_files = T.let({}, T::Hash[String, FoundFile])
+        @found_files =
+          T.let(
+            T::Hash[String, FoundFile].new do |h, filename|
+              if found_file = find_file(filename)
+                h[filename] = found_file
+              end
+            end,
+            T::Hash[String, FoundFile]
+          )
+        @memory_cache_max_size = memory_cache_max_size
+        @memory_cache = T.let({}, T::Hash[String, String])
       end
 
       sig do
@@ -28,45 +45,66 @@ module Mayu
         )
       end
       def serve(filename, accept_encodings: [])
-        found_file = get_file(filename)
+        found_file = @found_files[filename]
+
+        unless found_file
+          raise Errors::FileNotFound, "Could not find file #{filename}"
+        end
 
         headers = {
           "cache-control" => "public, max-age=#{CACHE_MAX_AGE}, immutable",
-          "content-type" => found_file.content_type
+          "content-type" => found_file.content_type,
+          "content-length" => found_file.size
         }
 
-        if found_file.has_brotli && accept_encodings.include?("br")
-          Protocol::HTTP::Response[
-            200,
-            { **headers, "content-encoding" => "br" },
-            Protocol::HTTP::Body::File.open(found_file.absolute_path + ".br")
-          ]
-        else
-          Protocol::HTTP::Response[
-            200,
-            headers,
-            Protocol::HTTP::Body::File.open(found_file.absolute_path)
-          ]
+        if accept_encodings.include?("br")
+          if brotlied = @found_files["#{filename}.br"]
+            return(
+              Protocol::HTTP::Response[
+                200,
+                {
+                  **headers,
+                  "content-encoding" => "br",
+                  "content-length" => brotlied.size
+                },
+                read_file(brotlied)
+              ]
+            )
+          end
         end
+
+        Protocol::HTTP::Response[200, headers, read_file(found_file)]
       end
 
       private
 
-      sig { params(filename: String).returns(FoundFile) }
-      def get_file(filename)
-        absolute_path = get_absolute_path(filename)
+      sig { params(filename: String).returns(T.nilable(FoundFile)) }
+      def find_file(filename)
+        absolute_path = File.join(@root_dir, filename)
 
-        @found_files[absolute_path] ||= find_file(absolute_path)
+        return unless File.exist?(absolute_path)
+
+        size = File.size(absolute_path)
+
+        content_type =
+          MIME::Types.type_for(absolute_path.delete_suffix(".br")).first.to_s
+
+        FoundFile.new(absolute_path:, content_type:, size:)
       end
 
-      sig { params(absolute_path: String).returns(FoundFile) }
-      def find_file(absolute_path)
-        raise Errors::FileNotFound unless File.exist?(absolute_path)
+      sig do
+        params(found_file: FoundFile).returns(
+          T.any(String, Protocol::HTTP::Body::File)
+        )
+      end
+      def read_file(found_file)
+        if found_file.size > @memory_cache_max_size
+          return Protocol::HTTP::Body::File.open(found_file.absolute_path)
+        end
 
-        has_brotli = File.exist?(absolute_path + ".br")
-        content_type = MIME::Types.type_for(absolute_path).first.to_s
-
-        FoundFile.new(absolute_path:, has_brotli:, content_type:)
+        @memory_cache[found_file.absolute_path] ||= begin
+          File.read(found_file.absolute_path)
+        end
       end
 
       sig { params(filename: String).returns(String) }
