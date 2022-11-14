53f8eeac2c94a1c538c03ef8cbbfad6a809b8db0
Andreas Alin <andreas.alin@gmail.com>
2022-11-13 20:39:25 -0500

    Add more icons

--- example/app/components/UI/Icon/generate-hash.rb
+++ example/app/components/UI/Icon/generate-hash.rb
@@ -4,4 +4,4 @@ Dir["./*.svg"].each do |file|
   puts "  #{File.basename(file, ".svg").delete_suffix("-solid").inspect} => svg(#{file.inspect}),"
 end
 
-puts "}.transform_keys(&:to_s).freeze"
+puts "}.freeze"
