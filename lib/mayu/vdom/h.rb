c102b9c98f2d4f5191304f64ea9e2140b150bd47
Andreas Alin <andreas.alin@gmail.com>
2023-01-22 03:01:20 -0500

    Cleanup

--- lib/mayu/vdom/h.rb
+++ lib/mayu/vdom/h.rb
@@ -5,21 +5,17 @@ require "sorbet-runtime"
 module Mayu
   module VDOM
     module H
-      class << self
-        extend T::Sig
+      extend T::Sig
 
-        sig do
-          params(
-            type: Component::ElementType,
-            children: T.any(Component::Children, Component::ChildType),
-            props: T.untyped
-          ).returns(Descriptor)
-        end
-        def [](type, *children, **props)
-          T.unsafe(Descriptor)[type, *children, **props]
-        end
-
-        alias h []
+      sig do
+        params(
+          type: Component::ElementType,
+          children: T.any(Component::Children, Component::ChildType),
+          props: T.untyped
+        ).returns(Descriptor)
+      end
+      def self.[](type, *children, **props)
+        T.unsafe(Descriptor)[type, *children, **props]
       end
     end
   end
