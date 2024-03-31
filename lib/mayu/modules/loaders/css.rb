178e1e8fb7da565fe3ee3cdb825bcb24de59606d
Andreas Alin <andreas.alin@gmail.com>
2024-03-31 10:10:51 -0500

    Don't print CSS source while transforming

--- lib/mayu/modules/loaders/css.rb
+++ lib/mayu/modules/loaders/css.rb
@@ -3,14 +3,15 @@ require_relative "transformers/css"
 module Mayu
   module Modules
     module Loaders
-      CSS = Data.define do
-        def call(loading_file)
-          loading_file
-            .maybe_load_source
-            .transform { Transformers::CSS.transform(_1.path, _1.source) }
-            .tap { puts _1.source }
+      CSS =
+        Data.define do
+          def call(loading_file)
+            loading_file.maybe_load_source.transform do
+              Transformers::CSS.transform(_1.path, _1.source)
+            end
+            # .tap { puts _1.source }
+          end
         end
-      end
     end
   end
 end
