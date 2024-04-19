8b88d7a097073388d05bc50e2bc1a93f5a5da548
Andreas Alin <andreas.alin@gmail.com>
2024-04-19 13:49:16 -0500

    Fix tests

--- lib/mayu/modules/loaders/transformers/__test__/css/basic.out.rb
+++ lib/mayu/modules/loaders/transformers/__test__/css/basic.out.rb
@@ -1,15 +1,15 @@
 Dep_ZuJKJG = import "./bar.png"
 Mayu::StyleSheet[
   source_filename: "/app/components/Test.css",
-  content_hash: "bUCOsdOoh2oMdYrVd3x2xh8hQpzIcrxTdeOZ6aF7sE0",
+  content_hash: "WjdqSJUhC23gOH-jtXdrLoIxIiKTcg3fDEOuw2Eo3XU",
   classes: {
-    __li: "/app/components/Test_li?K2gZASIc",
-    __ul: "/app/components/Test_ul?K2gZASIc",
-    bar: "/app/components/Test.bar?K2gZASIc",
-    foo: "/app/components/Test.foo?K2gZASIc",
-    "foo-bar": "/app/components/Test.foo-bar?K2gZASIc"
+    __li: "/app/components/Test_li?R-Dxlzoq",
+    __ul: "/app/components/Test_ul?R-Dxlzoq",
+    bar: "/app/components/Test.bar?R-Dxlzoq",
+    foo: "/app/components/Test.foo?R-Dxlzoq",
+    "foo-bar": "/app/components/Test.foo-bar?R-Dxlzoq"
   },
   content: <<CSS
-.\\/app\\/components\\/Test_ul\\?K2gZASIc{background:#0080ff80}.\\/app\\/components\\/Test_li\\?K2gZASIc,.\\/app\\/components\\/Test\\.foo\\?K2gZASIc{border:1px solid #f0f}.\\/app\\/components\\/Test\\.bar\\?K2gZASIc{background:url("#{Mayu::StyleSheet.encode_url(Dep_ZuJKJG.public_path)}")}.\\/app\\/components\\/Test\\.foo-bar\\?K2gZASIc{font-weight:700}
+.\\/app\\/components\\/Test_ul\\?R-Dxlzoq{background:#0080ff80}.\\/app\\/components\\/Test_li\\?R-Dxlzoq,.\\/app\\/components\\/Test\\.foo\\?R-Dxlzoq{border:1px solid #f0f}.\\/app\\/components\\/Test\\.bar\\?R-Dxlzoq{background:url("#{Mayu::StyleSheet.encode_url(Dep_ZuJKJG.public_path)}")}.\\/app\\/components\\/Test\\.foo-bar\\?R-Dxlzoq{font-weight:700}
 CSS
 ]
