6120f7eca8c59910b08f0fba71e8ab25443684bf
Andreas Alin <andreas.alin@gmail.com>
2024-04-29 23:12:09 -0500

    Make ConnectionStatus a type

--- lib/mayu/client/src/ping.ts
+++ lib/mayu/client/src/ping.ts
@@ -2,6 +2,8 @@ import type MayuPing from "./custom-elements/mayu-ping";
 
 import("./custom-elements/mayu-ping");
 
+export type ConnectionStatus = "disconnected" | "connected" | "transferring";
+
 function getPingElement() {
   return document.querySelector<MayuPing>("mayu-ping");
 }
@@ -10,8 +12,6 @@ export function updatePing(value: number) {
   getPingElement()?.setAttribute("ping", `${value.toFixed(2)}ms`);
 }
 
-export function updateConnectionStatus(
-  status: "disconnected" | "connected" | "transferring",
-) {
+export function updateConnectionStatus(status: ConnectionStatus) {
   getPingElement()?.setAttribute("status", status);
 }
