5cd81abaac08ba3f19490258a264b6718933b171
Andreas Alin <andreas.alin@gmail.com>
2024-07-25 13:24:19 -0500

    Fix assertion message

--- lib/mayu/modules/loaders/java_script.test.rb
+++ lib/mayu/modules/loaders/java_script.test.rb
@@ -14,7 +14,6 @@ class Mayu::Modules::Loaders::Haml::Test < Minitest::Test
   Dir[File.join(__dir__, "__test__", "js", "*.js")].each do |js|
     basename = File.basename(js, ".*")
     ruby = File.join(File.dirname(js), basename + ".rb")
-
     root = File.join(__dir__, "__test__", "js")
 
     define_method :"test_#{basename}" do
@@ -24,14 +23,11 @@ class Mayu::Modules::Loaders::Haml::Test < Minitest::Test
         ).source
 
       if File.exist?(ruby)
-        expected = File.read(ruby)
-
-        begin
-          assert_equal(expected, output)
-        rescue Minitest::Assertion
-          puts "\e[31m%s does not match %s\e[0m" % [ruby, js]
-          raise
-        end
+        assert_equal(
+          File.read(ruby),
+          output,
+          "#{ruby} doesn't match transformed output"
+        )
       else
         puts "\e[33mWriting #{ruby}\e[0m"
         File.write(ruby, output)
