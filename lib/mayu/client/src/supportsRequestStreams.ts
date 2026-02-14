d2d197a60c5462847cf32b9173fe1911a5412815
Andreas Alin <andreas.alin@gmail.com>
2026-02-14 10:14:11 -0500

    Fix exception in test

--- lib/mayu/client/src/supportsRequestStreams.ts
+++ lib/mayu/client/src/supportsRequestStreams.ts
@@ -2,16 +2,20 @@ function supportsRequestStreams() {
   // https://developer.chrome.com/articles/fetch-streaming-requests/#feature-detection
   let duplexAccessed = false;
 
-  const hasContentType = new Request("", {
-    body: new ReadableStream(),
-    method: "POST",
-    get duplex() {
-      duplexAccessed = true;
-      return "half";
-    },
-  } as any).headers.has("Content-Type");
+  try {
+    const hasContentType = new Request("/", {
+      body: new ReadableStream(),
+      method: "POST",
+      get duplex() {
+        duplexAccessed = true;
+        return "half";
+      },
+    } as any).headers.has("Content-Type");
 
-  return duplexAccessed && !hasContentType;
+    return duplexAccessed && !hasContentType;
+  } catch {
+    return false;
+  }
 }
 
 export default supportsRequestStreams();
