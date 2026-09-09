ac6edb52ec58a589174dde38f5f102f5610f044e
Andrés Alin <andreas.alin@gmail.com>
2026-09-08 21:58:49 -0500

    Add tests for TodoApp.haml

---
+++ example/app/pages/demos/todo/TodoApp.test.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+TodoApp = import("./TodoApp.haml")
+
+def test_manages_todos
+  screen = render(TodoApp)
+
+  assert_equal("0 items left", screen.get_by_text("0 items left").text)
+  assert_empty(screen.query_all_by_role(:listitem))
+
+  submit_todo(screen, "Write a test")
+  submit_todo(screen, "Ship the feature")
+
+  assert_equal(
+    ["Ship the feature", "Write a test"],
+    screen.get_all_by_role(:listitem).map { it.get_by_css("p").text }
+  )
+  assert_equal("2 items left", screen.get_by_text("2 items left").text)
+
+  write_test = screen.get_by_text("Write a test")
+  checkbox = write_test.at_xpath("../input[@type='checkbox']")
+  change_checkbox(screen, checkbox, checked: true)
+
+  assert_includes(screen.get_by_text("Write a test")["class"], "completed")
+  assert_equal("1 item left", screen.get_by_text("1 item left").text)
+
+  screen.get_by_role(:button, name: "Completed").click
+
+  assert_equal(
+    ["Write a test"],
+    screen.get_all_by_role(:listitem).map { it.get_by_css("p").text }
+  )
+
+  screen.get_by_role(:button, name: "Clear completed").click
+
+  assert_empty(screen.query_all_by_role(:listitem))
+  assert_equal("1 item left", screen.get_by_text("1 item left").text)
+end
+
+private
+
+def submit_todo(screen, description)
+  form = screen.get_by_css("form")
+  screen.callback(
+    callback_id(form, :onsubmit),
+    { target: { formData: { new_todo: description } } }
+  )
+end
+
+def change_checkbox(screen, checkbox, checked:)
+  target = { name: checkbox["name"], checked: }
+  screen.callback(
+    callback_id(checkbox, :onchange),
+    { target:, currentTarget: target }
+  )
+end
+
+def callback_id(node, event)
+  node[event]
+    .match(/\AMayu\.callback\(event,'(?<id>[^']+)'\)\z/)
+    .then { it[:id] }
+end
