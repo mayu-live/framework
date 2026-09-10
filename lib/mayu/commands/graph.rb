923df408652404c0aaef5833d837da32b79024c6
Andrés Alin <andreas.alin@gmail.com>
2026-09-10 15:44:49 -0500

    Add SVG output to graph command

--- lib/mayu/commands/graph.rb
+++ lib/mayu/commands/graph.rb
@@ -2,14 +2,49 @@
 # frozen_string_literal: true
 
 require "klenod/build/cli"
+require "open3"
 
 module Mayu
   module Commands
     class Graph < ::Klenod::Build::CLI::Graph
+      class GraphvizUnavailableError < StandardError
+      end
+
       self.description = ::Klenod::Build::CLI::Graph.description.gsub(
         "Klenod",
         "Mayu"
       )
+
+      options do
+        option "--svg", "Render the graph as SVG using Graphviz."
+      end
+
+      one :bundle_path, "Path to a Mayu runtime bundle.", default: "app.mayu-bundle"
+
+      def call
+        bundle = ::Klenod::Runtime.load_bundle(bundle_path)
+        dot =
+          ::Klenod::Build::Graphviz.call(
+            bundle,
+            include_assets: !options[:no_assets],
+            include_internal_virtual_modules: options[:internal_virtual_modules]
+          )
+        output.write(options[:svg] ? render_svg(dot) : dot)
+      rescue GraphvizUnavailableError => error
+        output.puts error.message
+        1
+      end
+
+      private
+
+      def render_svg(dot)
+        svg, error, status = Open3.capture3("dot", "-Tsvg", stdin_data: dot)
+        return svg if status.success?
+
+        raise GraphvizUnavailableError, "Graphviz could not generate an SVG: #{error.strip}"
+      rescue Errno::ENOENT
+        raise GraphvizUnavailableError, "Graphviz is required to generate an SVG. Install the dot command and try again."
+      end
     end
   end
 end
--- lib/mayu/commands/graph.test.rb
+++ lib/mayu/commands/graph.test.rb
@@ -25,6 +25,54 @@ class Mayu::Commands::GraphTest < Minitest::Test
     end
   end
 
+  def test_defaults_to_the_standard_mayu_bundle_path
+    Dir.mktmpdir("mayu-graph") do |root|
+      FileUtils.mkdir_p(File.join(root, "app"))
+      File.write(File.join(root, "app", "root.haml"), "%p Hello\n")
+      Mayu::Klenod::Configuration.new(root:).build(output: File.join(root, "app.mayu-bundle"))
+
+      output = StringIO.new
+      Dir.chdir(root) { Mayu::Commands::Application.new(["graph"], output:).call }
+
+      assert(output.string.start_with?("digraph klenod"))
+    end
+  end
+
+  def test_can_render_a_bundle_as_svg
+    Dir.mktmpdir("mayu-graph") do |root|
+      FileUtils.mkdir_p(File.join(root, "app"))
+      File.write(File.join(root, "app", "root.haml"), "%p Hello\n")
+      bundle_path = File.join(root, "app.mayu-bundle")
+      Mayu::Klenod::Configuration.new(root:).build(output: bundle_path)
+
+      output = StringIO.new
+      command = Mayu::Commands::Graph.new(["--svg", bundle_path], output:)
+      command.define_singleton_method(:render_svg) { |dot| "<svg>#{dot.length}</svg>" }
+
+      command.call
+
+      assert_match(/\A<svg>\d+<\/svg>\z/, output.string)
+    end
+  end
+
+  def test_explains_when_graphviz_is_unavailable
+    Dir.mktmpdir("mayu-graph") do |root|
+      FileUtils.mkdir_p(File.join(root, "app"))
+      File.write(File.join(root, "app", "root.haml"), "%p Hello\n")
+      bundle_path = File.join(root, "app.mayu-bundle")
+      Mayu::Klenod::Configuration.new(root:).build(output: bundle_path)
+
+      output = StringIO.new
+      command = Mayu::Commands::Graph.new(["--svg", bundle_path], output:)
+      command.define_singleton_method(:render_svg) do |_dot|
+        raise Mayu::Commands::Graph::GraphvizUnavailableError, "Graphviz is required to generate an SVG."
+      end
+
+      assert_equal(1, command.call)
+      assert_equal("Graphviz is required to generate an SVG.\n", output.string)
+    end
+  end
+
   def test_uses_mayu_in_its_description
     assert_equal(
       "Export a Mayu runtime bundle as Graphviz DOT.",
